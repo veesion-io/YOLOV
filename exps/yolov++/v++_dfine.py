5151cead1f09a7bc7e34a4fa64cc527822cebdab
hyperfraise <damien.menigaux@polytechnique.edu>
2025-03-10 17:40:59 +0100

    s

--- exps/yolov++/v++_dfine.py
+++ exps/yolov++/v++_dfine.py
@@ -64,7 +64,7 @@ class Exp(MyExp):
 
     def get_model(self):
         # rewrite get model func from yolox
-        in_channels = [256, 256, 256]  # D-FINE
+        in_channels = [384, 384, 384]  # D-FINE
         # backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels)
 
         """Load model EXACTLY like the working script"""
