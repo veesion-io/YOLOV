bbe9139f02e3bffcbc76367319120399a1d80709
hyperfraise <damien.menigaux@polytechnique.edu>
2025-03-10 17:36:39 +0100

    s

--- yolox/models/yolov_plus.py
+++ yolox/models/yolov_plus.py
@@ -4,6 +4,7 @@
 
 import torch.nn as nn
 
+
 class YOLOV(nn.Module):
     """
     YOLOX model module. The module list is defined by create_yolov3_modules function.
@@ -16,17 +17,22 @@ class YOLOV(nn.Module):
         self.backbone = backbone
         self.head = head
 
-    def forward(self, x, targets=None,nms_thresh=0.5,lframe=0,gframe=32):
+    def forward(self, x, targets=None, nms_thresh=0.5, lframe=0, gframe=32):
         # fpn output content features of [dark3, dark4, dark5]
-        fpn_outs = self.backbone(x)
+        fpn_outs = self.backbone(x, (960, 960))
         if self.training:
             assert targets is not None
-            loss, iou_loss, conf_loss, cls_loss, l1_loss,num_fg, \
-            loss_refined_cls,\
-            loss_refined_iou,\
-            loss_refined_obj = self.head(
-                fpn_outs, targets, x, lframe=lframe,gframe=gframe
-            )
+            (
+                loss,
+                iou_loss,
+                conf_loss,
+                cls_loss,
+                l1_loss,
+                num_fg,
+                loss_refined_cls,
+                loss_refined_iou,
+                loss_refined_obj,
+            ) = self.head(fpn_outs, targets, x, lframe=lframe, gframe=gframe)
             outputs = {
                 "total_loss": loss,
                 "iou_loss": iou_loss,
@@ -34,12 +40,18 @@ class YOLOV(nn.Module):
                 "conf_loss": conf_loss,
                 "cls_loss": cls_loss,
                 "num_fg": num_fg,
-                "loss_refined_cls":loss_refined_cls,
-                "loss_refined_iou":loss_refined_iou,
-                "loss_refined_obj":loss_refined_obj
+                "loss_refined_cls": loss_refined_cls,
+                "loss_refined_iou": loss_refined_iou,
+                "loss_refined_obj": loss_refined_obj,
             }
         else:
-
-            outputs = self.head(fpn_outs,targets,x,nms_thresh=nms_thresh, lframe=lframe,gframe=gframe)
+            outputs = self.head(
+                fpn_outs,
+                targets,
+                x,
+                nms_thresh=nms_thresh,
+                lframe=lframe,
+                gframe=gframe,
+            )
 
         return outputs
